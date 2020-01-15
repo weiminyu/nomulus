871b4c481f8dc9035faf92a4473edb70524fb90a
Weimin Yu <weiminyu@google.com>
2020-01-14 19:22:14 -0500

    Test

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -7,7 +7,8 @@ cd ${KOKORO_ARTIFACTS_DIR}/github/nomulus
 gcloud auth activate-service-account \
     --key-file="$KOKORO_GFILE_DIR/kokoro-gcs-by-gsutil.json"
 
-GOOGLE_APPLICATION_CREDENTIALS="$KOKORO_GFILE_DIR/kokoro-gcs-by-gsutil.json"
+export \
+    GOOGLE_APPLICATION_CREDENTIALS="$KOKORO_GFILE_DIR/kokoro-gcs-by-gsutil.json"
 
 ./integration/runCompatibilityTests.sh -p domain-registry-dev -s sql
 ./integration/runCompatibilityTests.sh -p domain-registry-dev -s nomulus
