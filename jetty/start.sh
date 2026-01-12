5725eb95e0bf0fbe19f7ecf799fa45901e9f3b94
Juan Celhay <jicelhay@google.com>
2026-01-12 10:19:05 -0500

    Add Cloud java profiler to nomulus docker images (#2919)
    * add cloud profiler to dockerfile and start script
    
    * add apt-get update
    
    * change in cb machine type for nomulus
    
    * fix typo
    
    * add max worker limit to gradle tests
    
    * Switch to root before doing apt-get
    
    * correct dockerfile
    
    * jetty/Dockerfile
    
    * profiler service conditional to kubernetes container name

--- jetty/start.sh
+++ jetty/start.sh
@@ -21,6 +21,8 @@ cd webapps
 find . -maxdepth 1 -type d -name "console-*" -exec rm -rf {} +
 cd /jetty-base
 echo "Running ${env}"
-java -Dgoogle.registry.environment=${env} \
+# Use the CONTAINER_NAME variable from Kubernetes YAML to set the profiler service name.
+java -agentpath:/opt/cprof/profiler_java_agent.so=-cprof_service=${CONTAINER_NAME},-cprof_enable_heap_sampling=true \
+    -Dgoogle.registry.environment=${env} \
     -Djava.util.logging.config.file=/logging.properties \
     -jar /usr/local/jetty/start.jar
